87225db365ba1bfbc83bc67a363908c493024fb1
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2023-03-04 20:08:25 +0100

    fix: balance

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,6 @@
 export const MAX_ACTION_POINTS = 10;
 export const DECK_SIZE = 4;
-export const TIME_FOR_ONE_ACTION_POINT = 2000;
+export const TIME_FOR_ONE_ACTION_POINT = 3000;
 
 export const SHUFFLE_PRICE = 3;
 
